52c7111d1fa39b157875d98d72474eea5f946de8
Cody O'Brien <cody@cdso.dev>
2023-09-13 18:03:15 -0400

    attempt at helmet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const cors = require("cors");
-
+const helmet = require("helmet");
 require("dotenv").config();
 
 const { PORT = 3001 } = process.env;
@@ -15,8 +15,8 @@ mongoose.connect(
   (e) => console.log("db error", e),
 );
 
+app.use(helmet());
 const routes = require("./routes");
-
 app.use(express.json());
 
 app.use(routes);
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 
-const { updateCurrentUser, getCurrentUser } = require("../controllers/user");
+const {
+  updateCurrentUser,
+  getCurrentUser,
+  createUser,
+  logIn,
+} = require("../controllers/user");
 
 // Create
-
+router.post("/signup", createUser);
 router.use(auth);
 // Read
 router.get("/me", getCurrentUser);
